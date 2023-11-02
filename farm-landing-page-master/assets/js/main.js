bcc5568faa2ca47bb84c093e954847a349d506c1
Saumya Sah <74109566+saumyasah@users.noreply.github.com>
2023-11-03 02:52:01 +0530

    Add files via upload

---
+++ farm-landing-page-master/assets/js/main.js
@@ -0,0 +1,7 @@
+import navigation from './navigation.js'
+import works from './works.js'
+import testimonials from './testimonials.js'
+console.log('main js reached')
+navigation();
+works();
+testimonials();
